d95acf752dc51cc2f831d60b741ccd1936f3b31f
rider <myintthwaykhine127@gmail.com>
2022-06-23 21:05:55 +0630

    fitler todos with nav button

---
+++ src/components/NavbarView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="btnGroup">
+    <button>
+      <span
+        @click="fetchData('all')"
+        :class="[current === 'all' ? 'active' : null]"
+        >All</span
+      >
+    </button>
+    <button>
+      <span
+        @click="fetchData('complete')"
+        :class="[current === 'complete' ? 'active' : null]"
+        >Complete</span
+      >
+    </button>
+    <button>
+      <span
+        @click="fetchData('ongoing')"
+        :class="[current === 'ongoing' ? 'active' : null]"
+        >Ongoing</span
+      >
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["current"],
+  emits: ["dataFetch"],
+  setup(_, context) {
+    let fetchData = (data) => {
+      context.emit("dataFetch", data);
+    };
+
+    return { fetchData };
+  },
+};
+</script>
+
+<style>
+.btnGroup > button {
+  background-color: #fff;
+  border: none;
+  color: #fafa;
+  cursor: pointer;
+  user-select: none;
+}
+
+.btnGroup {
+  margin-bottom: 20px;
+}
+
+.active {
+  color: #555;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <div v-for="todo in todos" :key="todo.id">
+    <navbar-view @dataFetch="dataFetch" :current="current" />
+    <div v-for="todo in todosFilter" :key="todo.id">
       <SingleTodo :todo="todo" @deleteTodoItem="deleteTodoItem" />
     </div>
   </div>
@@ -9,11 +10,16 @@
 <script>
 import getTodos from "@/composables/getTodos";
 import SingleTodo from "@/views/SingleTodo.vue";
+import NavbarView from "@/components/NavbarView.vue";
+import { ref } from "@vue/reactivity";
+import { computed } from "@vue/runtime-core";
 
 export default {
   name: "HomeView",
-  components: { SingleTodo },
+  components: { SingleTodo, NavbarView },
   setup() {
+    let current = ref("all");
+
     let { load, todos, errors } = getTodos();
 
     let deleteTodoItem = (id) => {
@@ -22,9 +28,29 @@ export default {
       });
     };
 
+    let dataFetch = (data) => {
+      current.value = data;
+    };
+
+    let todosFilter = computed(() => {
+      if (current.value === "complete") {
+        return todos.value.filter((todo) => {
+          return todo.complete;
+        });
+      }
+
+      if (current.value === "ongoing") {
+        return todos.value.filter((todo) => {
+          return !todo.complete;
+        });
+      }
+
+      return todos.value;
+    });
+
     load();
 
-    return { errors, todos, deleteTodoItem };
+    return { errors, todos, deleteTodoItem, dataFetch, current, todosFilter };
   },
 };
 </script>
